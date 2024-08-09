7e5fe6dd74837608632683f0ed35dca67f78a0c0
Tyler Ortolano <tylerortolano@Tylers-MacBook-Pro.local>
2024-08-09 17:39:49 -0400

    43

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
     <Box sx={{ bgcolor: '#333', color: '#fff', py: 3, mt: 4 }}>
       <Container maxWidth="lg">
         <Typography variant="body1" align="center">
-          &copy; 2024 MyStore. All rights reserved.
+          &copy; 2024 GOLF.store. All rights reserved.
         </Typography>
         <Box sx={{ mt: 2, textAlign: 'center' }}>
           <Link href="#" color="inherit" sx={{ mx: 2 }}>
